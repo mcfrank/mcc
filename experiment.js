f876a499f13d0134ca3eb1a01908da65b0d0d20b
Long Ouyang <longouyang@gmail.com>
2011-11-10 14:46:15 -0800

    Incorporate Noah and Dan's comments

--- experiment.js
+++ experiment.js
@@ -1,14 +1,24 @@
-// I'm implementing the experiment using a data structure that I call a **stream**. The insight behind streams is that many experiments consists of a sequence of largely homogeneous trials that vary based on a parameter. For instance, in this example experiment, a lot stays the same from trial to trial - we always have to present some number, the subject always has to make a response, and we always want to record that response. Of course, the trials do differ - we're displaying a different number every time. The idea behind the stream is to separate what stays the same from what differs - to factor out the constants. This results in **parametric code**, which is much easier to maintain - it's simple to add, remove, or change conditions, do randomization, and do testing.
+// I'm implementing the experiment using a data structure that I call a **sequence**. The insight behind sequences is that many experiments consist of a sequence of largely homogeneous trials that vary based on a parameter. For instance, in this example experiment, a lot stays the same from trial to trial - we always have to present some number, the subject always has to make a response, and we always want to record that response. Of course, the trials do differ - we're displaying a different number every time. The idea behind the sequence is to separate what stays the same from what differs - to factor out the constants. This results in **parametric code**, which is much easier to maintain - it's simple to add, remove, or change conditions, do randomization, and do testing.
+
+// ## High-level overview
+// Things happen in this order:
+// 
+// 1. Compute randomization parameters (which keys to press for even/odd and trial order), fill in the template <code>{{}}</code> slots that indicate which keys to press for even/odd, and show the instructions slide.
+// 2. Set up the experiment sequence object.
+// 3. When the subject clicks the start button, it calls <code>experiment.next()</code>
+// 4. <code>experiment.next()</code> checks if there are any trials left to do. If there aren't, it calls <code>experiment.end()</code>, which shows the finish slide, waits for 1.5 seconds, and then uses mmturkey to submit to Turk.
+// 5. If there are more trials left, <code>experiment.next()</code> shows the next trial, records the current time for computing reaction time, and sets up a listener for a key press.
+// 6. The key press listener, when it detects either a P or a Q, constructs a data object, which includes the presented stimulus number, RT (current time - start time), and whether or not the subject was correct. This entire object gets pushed into the <code>experiment.data</code> array. Then we show a blank screen and wait 500 milliseconds before calling <code>experiment.next()</code> again.
 
 // ## Helper functions
 
 // Shows slides. We're using jQuery here - the **$** is the jQuery selector function, which takes as input either a DOM element or a CSS selector string.
 function showSlide(id) {
+  // Hide all slides
 	$(".slide").hide();
+	// Show just the slide we want to show
 	$("#"+id).show();
 }
-
-showSlide("instructions");
 
 // Get random integers.
 // When called with no arguments, it returns either 0 or 1. When called with one argument, *a*, it returns a number in [*0*,*a-1*]. When called with two arguments, *a* and *b*, returns a random value in [*a*,*b*].
@@ -28,45 +38,43 @@ Array.prototype.random = function() {
 
 // ## Configuration settings
 var allKeyBindings = [
-  {"p": "odd", "q": "even"},
-  {"p": "even", "q": "odd"}
-];
-
-var allTrialOrders = [
-  [1,3,2,5,4,9,8,7,6],
-  [8,4,3,7,5,6,2,1,9]
-];
-
-var myKeyBindings = allKeyBindings.random(),
-    myTrialOrder = allTrialOrders.random();
+      {"p": "odd", "q": "even"},
+      {"p": "even", "q": "odd"} ],
+    allTrialOrders = [
+      [1,3,2,5,4,9,8,7,6],
+      [8,4,3,7,5,6,2,1,9] ],
+    myKeyBindings = allKeyBindings.random(),
+    myTrialOrder = allTrialOrders.random(),
+    pOdd = (myKeyBindings["p"] == "odd");
     
 // Fill in the instructions template. In particular,
 // let the subject know which keys correspond to even/odd.
-var pOdd = (myKeyBindings["p"] == "odd");
+
 $("#odd-key").html(pOdd ? "P" : "Q");
 $("#even-key").html(pOdd ? "Q" : "P");
 
+// Show the instructions slide -- this is what we want subjects to see first.
+showSlide("instructions");
+
 
 // ## The main event
-// I implement the stream as an object with properties and methods. The benefit of encapsulating everything in an object is that it's conceptually coherent (i.e. the <code>data</code> variable belongs to this particular stream and not any other) and allows you to **compose** streams to build more complicated experiments. For instance, if you wanted an experiment with, say, a survey, a reaction time test, and a memory test presented in a number of different orders, you could easily do so by creating three separate streams and dynamically setting the <code>end()</code> function for each stream.
+// I implement the sequence as an object with properties and methods. The benefit of encapsulating everything in an object is that it's conceptually coherent (i.e. the <code>data</code> variable belongs to this particular sequence and not any other) and allows you to **compose** sequences to build more complicated experiments. For instance, if you wanted an experiment with, say, a survey, a reaction time test, and a memory test presented in a number of different orders, you could easily do so by creating three separate sequences and dynamically setting the <code>end()</code> function for each sequence so that it points to the next. **More practically, you should stick everything in an object and submit that whole object so that you don't lose data (e.g. randomization parameters, what condition the subject is in, etc). Don't worry about the fact that some of the object properties are functions -- mmturkey (the Turk submission library) will strip these out.**
 
 var experiment = {
-  // Parameters for this stream.
+  // Parameters for this sequence.
   trials: myTrialOrder,
-  // Already completed parameters.
-  completed: [],
   // Experiment-specific parameters - which keys map to odd/even
   keyBindings: myKeyBindings,
   // An array to store the data that we're collecting.
   data: [],
-  // The function that gets called when the stream is finished.
+  // The function that gets called when the sequence is finished.
   end: function() {
     // Show the finish slide.
     showSlide("finished");
     // Wait 1.5 seconds and then submit the whole experiment object to Mechanical Turk (mmturkey filters out the functions so we know we're just submitting properties [i.e. data])
     setTimeout(function() { turk.submit(experiment) }, 1500);
   },
-  // The work horse of the stream - what to do on every trial.
+  // The work horse of the sequence - what to do on every trial.
   next: function() {
     // Get the current trial - <code>shift()</code> removes the first element of the array and returns it.
     var n = experiment.trials.shift();
@@ -74,8 +82,6 @@ var experiment = {
     if (typeof n == "undefined") {
       return experiment.end();
     }
-    
-    experiment.completed.push(n);
     
     // Compute the correct answer. I'm using the so-called **ternary operator**, which is a shorthand for <code>if (...) { ... } else { ... }</code>
     var realParity = (n % 2 == 0) ? "even" : "odd";
@@ -99,13 +105,12 @@ var experiment = {
         // Record the end time, which key was pressed, and what that means (even or odd).
         var endTime = (new Date()).getTime();
             key = (keyCode == 80) ? "p" : "q",
-            userParity = experiment.keyBindings[key];
-            
-        var data = {
-          stimulus: n,
-          accuracy: realParity == userParity ? 1 : 0,
-          rt: endTime - startTime
-        };
+            userParity = experiment.keyBindings[key],
+            data = {
+              stimulus: n,
+              accuracy: realParity == userParity ? 1 : 0,
+              rt: endTime - startTime
+            };
         
         experiment.data.push(data);
         // Temporarily clear the number.
