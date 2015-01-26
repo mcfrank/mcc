1ecd86c874274eb6b600f9103100519f308cf7d9
Long Ouyang <longouyang@gmail.com>
2015-01-26 12:03:13 -0800

    Slightly smarter text folding

--- _layout/custom.js
+++ _layout/custom.js
@@ -1,8 +1,22 @@
-$(function() {
+setTimeout(function() {
+
+  var getLineHeight = function(el) {
+    return parseInt(document
+                    .defaultView
+                    .getComputedStyle(el, null)["line-height"]
+                    .replace("px",""));
+  }
   
   $("li:has(.content) > .annotation")
-    .filter(function(i, x) {
-      return $(x).height() > 40 })
+    .filter(function(i, a) {
+      var p = $(a).find("p")[0];
+      if (!p) {
+        return false;
+      }
+      
+      var c = $(this).parent().find(".content")[0];
+      return $(p).height() > $(c).outerHeight();
+    })
     .addClass("folded")
     .prepend($("<span class='ellipsis'>+</span>").click(function(e) {
       $(this).parent().toggleClass("folded");
@@ -14,4 +28,4 @@ $(function() {
       }
     }))
 
-});
+}, 200);
